2adae9508b9522e77461cf02ee69a1db2dc7c623
NORZACO AS <norzaco@gmail.com>
2023-02-14 09:55:56 +0100

    install MongoDB and use it to create a simple CRUD

--- index.js
+++ index.js
@@ -1,20 +1,112 @@
-const mongoClient = require('mongodb').MongoClient
+const mongoClient = require('mongodb').MongoClient;
+//dotenv
+require('dotenv').config();
 
 
-const url = 'mongodb://127.0.0.1:27017';
-const dbname = 'TestDatabase';
+const url = process.env.MONGO_URL;
+const dbname = process.env.DATANAME
 
 
 
-mongoClient.connect(url, {}, (error, client) => {
-  if(error)
-    console.log("Cannot connect");
-  console.log("Connection OK");
-  const db = client.db(dbname);
 
-  db.collection('Users').find({
-    firstName: "John"
-  }).toArray((error, result) => {
-    console.log(result);
+function DisplayUsers(params) {
+  mongoClient.connect(url, {}, (error, client) => {
+    if (error)
+      console.log("Cannot connect");
+    console.log("Connection OK");
+    const db = client.db(dbname);
+  
+    db.collection('Users').find({
+      firstName: "John"
+    }).toArray((error, result) => {
+      console.log(result);
+      console.log('----');
+    });
   });
-});
+}
+
+
+// display_the_user_data()
+// addUser()
+// DisplayUsers()
+// rename_all_Johns_toToms()
+
+function display_the_user_data(params) {
+  mongoClient.connect(url, {}, (error, client) => {
+    if (error)
+      console.log("Cannot connect");
+    console.log("Connection OK");
+    const db = client.db(dbname);
+  
+    db.collection('Users').find({
+      firstName: "John"
+    }, {
+      projection: {
+        _id: false,
+        firstName: true,
+        lastName: true
+      }
+    }).toArray((error, result) => {
+      console.log(result);
+      console.log('-------');
+    });
+  });
+}
+
+
+function addUser() {
+  mongoClient.connect(url, {}, (error, client) => {
+    if (error)
+      console.log("Cannot connect");
+    console.log("Connection OK");
+    const db = client.db(dbname);
+  
+    db.collection('Users').insertOne({
+      firstName: 'Johnny',
+      lastName: 'Brown'
+    }, (error, result) => {
+      if (error)
+        console.log('Cannot add user');
+      console.log(result);
+    })
+  });
+}
+
+
+function rename_all_Johns_toToms() {
+  mongoClient.connect(url, {}, (error, client) => {
+    if(error)
+      console.log("Cannot connect");
+    console.log("Connection OK");
+    const db = client.db(dbname);
+    db.collection('Users').updateMany({
+      firstName: 'John'
+    }, {
+      $set: {firstName: 'Tom'}
+    }, (error, result) => {
+      if(error)
+        console.log('Cannot update users');
+      console.log(result);
+    })
+  });
+}
+
+
+
+// delete
+function delete_all_Johnny() {
+  mongoClient.connect(url, {}, (error, client) => {
+    if(error)
+      console.log("Cannot connect");
+    console.log("Connection OK");
+    const db = client.db(dbname);
+    db.collection('Users').deleteMany({
+      firstName: 'Johnny'
+    }, (error, result) => {
+      if(error)
+        console.log('Cannot delete users');
+      console.log(result);
+    })
+  });
+}
+// delete_all_Johnny()
